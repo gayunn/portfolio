5996ec2375480fdbe01f847a0f0998400b0a5331
gayun66 <83623554+gayun66@users.noreply.github.com>
2024-05-22 10:23:31 +0900

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
-const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const cleanCss = require("gulp-clean-css"); // ★★★★ add
 
-function minifyCSS() {
-    return gulp.src('src/css/*.css')
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('dist/css'));
-}
+gulp.task("create-clean-css", function(){
+    return gulp
+        .src("src/css/*.css")
+        .pipe(concat("style.css"))
+        .pipe(cleanCss({ compatibiliy: 'ie8' })) // ★★★★ add
+        .pipe(gulp.dest("dist/css"))
+});
 
-exports.minifyCSS = minifyCSS;
+gulp.task(
+    "default",
+    gulp.parallel("create-clean-css") 
+);
