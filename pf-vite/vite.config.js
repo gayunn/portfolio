fa22b4389fd02cdde27b9098b66ed30bdc1ae15c
gayun66 <83623554+gayun66@users.noreply.github.com>
2024-06-19 09:28:33 +0900

    vite개발 환경_Update

--- pf-vite/vite.config.js
+++ pf-vite/vite.config.js
@@ -1,6 +1,17 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  
   root: "src/markup/",
   build: {
       outDir: "../public",
   },
-};
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/scss/_variables";' 
+      }
+    }
+  }
+});
