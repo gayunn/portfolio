05b68961de18f5af74c24193667ec87597b32990
gayun66 <belllaxix@gmail.com>
2024-06-30 15:41:46 +0900

    wip

--- Vue-Pf/src/main.js
+++ Vue-Pf/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import portfolio from './index.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(portfolio),
 }).$mount('#app')
